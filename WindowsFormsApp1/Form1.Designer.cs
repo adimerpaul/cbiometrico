93b108e94d0bcb3d9c8f72dbb94b83541e07f2f9
adimerpaul <adimer101@gmail.com>
2022-10-10 19:05:17 -0400

    proyecto biometrico en c

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.deviceSerial = new System.Windows.Forms.Label();
+            this.prompt = new System.Windows.Forms.Label();
+            this.fpicture = new System.Windows.Forms.PictureBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.txtTemplate = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fpicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // deviceSerial
+            // 
+            this.deviceSerial.AutoSize = true;
+            this.deviceSerial.Location = new System.Drawing.Point(476, 49);
+            this.deviceSerial.Name = "deviceSerial";
+            this.deviceSerial.Size = new System.Drawing.Size(46, 17);
+            this.deviceSerial.TabIndex = 0;
+            this.deviceSerial.Text = "label1";
+            // 
+            // prompt
+            // 
+            this.prompt.AutoSize = true;
+            this.prompt.Location = new System.Drawing.Point(476, 271);
+            this.prompt.Name = "prompt";
+            this.prompt.Size = new System.Drawing.Size(46, 17);
+            this.prompt.TabIndex = 1;
+            this.prompt.Text = "label1";
+            // 
+            // fpicture
+            // 
+            this.fpicture.Location = new System.Drawing.Point(479, 80);
+            this.fpicture.Name = "fpicture";
+            this.fpicture.Size = new System.Drawing.Size(221, 177);
+            this.fpicture.TabIndex = 2;
+            this.fpicture.TabStop = false;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(41, 12);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(100, 33);
+            this.btnRegister.TabIndex = 3;
+            this.btnRegister.Text = "Registrar";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Location = new System.Drawing.Point(699, 13);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(92, 32);
+            this.btnVerify.TabIndex = 4;
+            this.btnVerify.Text = "Verificar";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // txtTemplate
+            // 
+            this.txtTemplate.Location = new System.Drawing.Point(479, 309);
+            this.txtTemplate.Multiline = true;
+            this.txtTemplate.Name = "txtTemplate";
+            this.txtTemplate.Size = new System.Drawing.Size(353, 111);
+            this.txtTemplate.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(41, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(323, 24);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Dedo 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Dedo 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 119);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 92);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 344);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Dedo 3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(44, 249);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(323, 92);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(44, 376);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(323, 92);
+            this.textBox3.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 471);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(320, 42);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "GUARDAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(398, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(398, 347);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(398, 376);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 525);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtTemplate);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.fpicture);
+            this.Controls.Add(this.prompt);
+            this.Controls.Add(this.deviceSerial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fpicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label deviceSerial;
+        private System.Windows.Forms.Label prompt;
+        private System.Windows.Forms.PictureBox fpicture;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.TextBox txtTemplate;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,349 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxZKFPEngXControl;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string conexion = "datasource=127.0.0.1;port=3306;username=root;password=;database=biometrico";
+        string[,] afiliados = new string[600, 10];
+        int afiliadosLen;
+        private AxZKFPEngX ZkFprint = new AxZKFPEngX();
+        private bool Check;
+        public Form1()
+        {
+            InitializeComponent();
+            afiliadosGet();
+        }
+        public void afiliadosGet()
+        {
+            MySqlConnection conexionDB = new MySqlConnection(conexion);
+            try
+            {
+                conexionDB.Open();
+                MySqlCommand command = new MySqlCommand("SELECT *, CONCAT(apellidos, ' ', nombres) AS nombrecompleto FROM afiliados ORDER BY nombrecompleto;", conexionDB);
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    afiliadosLen = 0;
+                    while (reader.Read())
+                    {
+                        afiliados[afiliadosLen, 0] = reader.GetString(0);
+                        afiliados[afiliadosLen, 1] = reader.GetString(11);
+                        afiliados[afiliadosLen, 2] = reader.GetString(5);
+                        afiliados[afiliadosLen, 3] = reader.GetString(6);
+                        afiliados[afiliadosLen, 4] = reader.GetString(7);
+                        afiliadosLen++;
+                    }
+                }
+                conexionDB.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = null;
+            comboBox1.Items.Clear();
+
+            comboBox1.DisplayMember = "nombre";
+            comboBox1.ValueMember = "id";
+            MySqlConnection conexionDB = new MySqlConnection(conexion);
+            conexionDB.Open();
+            try
+            {
+                MySqlCommand comando = new MySqlCommand("SELECT *, CONCAT(apellidos, ' ', nombres) AS nombrecompleto FROM afiliados ORDER BY nombrecompleto;", conexionDB);
+                MySqlDataAdapter data = new MySqlDataAdapter(comando);
+                DataTable dt = new DataTable();
+                data.Fill(dt);
+                comboBox1.ValueMember = "id";
+                comboBox1.DisplayMember = "nombrecompleto";
+                comboBox1.DataSource = dt;
+                conexionDB.Close();
+            } catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            Controls.Add(ZkFprint);
+            InitialAxZkfp();
+            //MySqlConnection connection = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=biometrico");
+            //MySqlCommand command = new MySqlCommand("SELECT * FROM afiliados ORDER BY apellidos",connection);
+            //try
+            //{
+            //connection.Open();
+            //MySqlDataReader reader = command.ExecuteReader();
+            //if (reader.HasRows)
+            //{
+            //while (reader.Read())
+            //{
+            //MessageBox.Show(reader.GetString(4));
+            //comboBox1.Items.Add(new { nombre = reader.GetString(4) + ' '+reader.GetString(3), id = reader.GetString(0) });
+            //}
+            //}
+            //connection.Close();
+            //}catch(Exception error)
+            //{
+            //    MessageBox.Show(error.ToString());
+            //}
+        }
+        private void InitialAxZkfp()
+        {
+            try
+            {
+
+                ZkFprint.OnImageReceived += zkFprint_OnImageReceived;
+                ZkFprint.OnFeatureInfo += zkFprint_OnFeatureInfo;
+                //zkFprint.OnFingerTouching 
+                //zkFprint.OnFingerLeaving
+                ZkFprint.OnEnroll += zkFprint_OnEnroll;
+
+                if (ZkFprint.InitEngine() == 0)
+                {
+                    ZkFprint.FPEngineVersion = "9";
+                    ZkFprint.EnrollCount = 3;
+                    deviceSerial.Text += " " + ZkFprint.SensorSN + " Count: " + ZkFprint.SensorCount.ToString() + " Index: " + ZkFprint.SensorIndex.ToString();
+                    ShowHintInfo("Device successfully connected");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ShowHintInfo("Device init err, error: " + ex.Message);
+            }
+        }
+        private void ShowHintInfo(String s)
+        {
+            prompt.Text = s;
+        }
+        private void zkFprint_OnImageReceived(object sender, IZKFPEngXEvents_OnImageReceivedEvent e)
+        {
+            Graphics g = fpicture.CreateGraphics();
+            Bitmap bmp = new Bitmap(fpicture.Width, fpicture.Height);
+            g = Graphics.FromImage(bmp);
+            int dc = g.GetHdc().ToInt32();
+            ZkFprint.PrintImageAt(dc, 0, 0, bmp.Width, bmp.Height);
+            g.Dispose();
+            fpicture.Image = bmp;
+        }
+
+        private void zkFprint_OnFeatureInfo(object sender, IZKFPEngXEvents_OnFeatureInfoEvent e)
+        {
+
+            String strTemp = string.Empty;
+            if (ZkFprint.EnrollIndex != 1)
+            {
+                if (ZkFprint.IsRegister)
+                {
+                    if (ZkFprint.EnrollIndex - 1 > 0)
+                    {
+                        int eindex = ZkFprint.EnrollIndex - 1;
+                        strTemp = "Please scan again ..." + eindex;
+                    }
+                }
+            }
+            ShowHintInfo(strTemp);
+        }
+        private void zkFprint_OnEnroll(object sender, IZKFPEngXEvents_OnEnrollEvent e)
+        {
+            if (e.actionResult)
+            {
+
+
+                string template = ZkFprint.EncodeTemplate1(e.aTemplate);
+                txtTemplate.Text = template;
+                ShowHintInfo("Registration successful. You can verify now");
+                //btnRegister.Enabled = false;
+                btnVerify.Enabled = true;
+            }
+            else
+            {
+                ShowHintInfo("Error, please register again.");
+
+            }
+        }
+        private void zkFprint_OnCapture(object sender, IZKFPEngXEvents_OnCaptureEvent e)
+        {
+            string template = ZkFprint.EncodeTemplate1(e.aTemplate);
+
+            for (int i = 0; i < afiliadosLen; i++)
+            {
+                if (ZkFprint.VerFingerFromStr(ref template, afiliados[i, 2], false, ref Check))
+                {
+                    ShowHintInfo(afiliados[i, 1]);
+                    registerPagos(afiliados[i, 0]);
+                    break;
+                }
+                else if (ZkFprint.VerFingerFromStr(ref template, afiliados[i, 3], false, ref Check))
+                {
+                    ShowHintInfo(afiliados[i, 1]);
+                    registerPagos(afiliados[i, 0]);
+                    break;
+                }
+                else if (ZkFprint.VerFingerFromStr(ref template, afiliados[i, 4], false, ref Check))
+                {
+                    ShowHintInfo(afiliados[i, 1]);
+                    registerPagos(afiliados[i, 0]);
+                    break;
+                }
+                else
+                {
+                    ShowHintInfo("Not Verified");
+                    //break;
+                }
+
+            }
+
+            //if (ZkFprint.VerFingerFromStr(ref template, txtTemplate.Text, false, ref Check))
+            //{
+            //    ShowHintInfo("Verified");
+            //}
+            //else
+            //    ShowHintInfo("Not Verified");
+
+        }
+        private void registerPagos(string id){
+
+            MySqlConnection conexionDB = new MySqlConnection(conexion);
+            try
+            {
+                String vehiculo_id = "";
+                String afiliado_id = "";
+                String grupo_id = "";
+                String monto = "0";
+                conexionDB.Open();
+                MySqlCommand command = new MySqlCommand("SELECT v.id,afiliado_id,grupo_id,monto FROM vehiculos v INNER JOIN grupos g ON v.grupo_id=g.id WHERE afiliado_id="+id+" LIMIT 1", conexionDB);
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        vehiculo_id= reader.GetString(0);
+                        afiliado_id = reader.GetString(1);
+                        grupo_id = reader.GetString(2);
+                        monto = reader.GetString(3);
+                    }
+                    
+
+                }
+                conexionDB.Close();
+                conexionDB.Open();
+                DateTime aDate = DateTime.Now;
+                string fecha = aDate.ToString("yyyy-MM-dd").ToString();
+                MySqlCommand command2 = new MySqlCommand("INSERT INTO pagos SET afiliado_id=" + afiliado_id + ", grupo_id=" + grupo_id + ", vehiculo_id=" + vehiculo_id + ",monto=" + monto + ",fecha='"+fecha+"'", conexionDB);
+                MySqlDataReader reader2 = command2.ExecuteReader();
+                conexionDB.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            ZkFprint.CancelEnroll();
+            ZkFprint.EnrollCount = 3;
+            ZkFprint.BeginEnroll();
+            ShowHintInfo("Please give fingerprint sample.");
+        }
+
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            if (ZkFprint.IsRegister)
+            {
+                ZkFprint.CancelEnroll();
+            }
+            ZkFprint.OnCapture += zkFprint_OnCapture;
+            ZkFprint.BeginCapture();
+            ShowHintInfo("Please give fingerprint sample.");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            MySqlConnection conexionDB = new MySqlConnection(conexion);
+            try
+            {
+                conexionDB.Open();
+                MySqlCommand command = new MySqlCommand("SELECT * FROM afiliados WHERE id =" + comboBox1.SelectedValue.ToString(), conexionDB);
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        textBox1.Text = reader.GetString(5);
+                        textBox2.Text = reader.GetString(6);
+                        textBox3.Text = reader.GetString(7);
+                    }
+                }
+                conexionDB.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //int selectedIndex = comboBox1.SelectedValue.ToString();
+            //comboBox1.SelectedItem.ToString();
+            //MessageBox.Show(comboBox1.SelectedValue.ToString());
+            //var o =new { id=1 };
+            //o = comboBox1.SelectedItem;
+            //Console.WriteLine(o.id);
+            //MessageBox.Show("Selected Type : " + comboBox1.SelectedIndex.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexionDB = new MySqlConnection(conexion);
+            try
+            {
+                conexionDB.Open();
+                MySqlCommand command = new MySqlCommand("UPDATE afiliados SET dedo1='"+textBox1.Text+ "',dedo2='" + textBox2.Text + "',dedo3='" + textBox3.Text + "' WHERE id =" + comboBox1.SelectedValue.ToString(), conexionDB);
+                MySqlDataReader reader = command.ExecuteReader();
+                conexionDB.Close();
+                afiliadosGet();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = txtTemplate.Text;
+            //txtTemplate.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = txtTemplate.Text;
+            //txtTemplate.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = txtTemplate.Text;
+            //txtTemplate.Text = "";
+        }
+    }
+}
